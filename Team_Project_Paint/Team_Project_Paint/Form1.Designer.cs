0ae42aad41695d2877db06482e7f8186d437b5e9
Vlada Dorozhko <dorozhko2v@gmail.com>
2021-04-30 21:13:15 +0300

    first commit
    first vision of paint

---
+++ Team_Project_Paint/Team_Project_Paint/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Team_Project_Paint
+{
+    partial class Paint
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Paint));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.DotButton = new System.Windows.Forms.ToolStripButton();
+            this.LineButton = new System.Windows.Forms.ToolStripButton();
+            this.CurveButton = new System.Windows.Forms.ToolStripButton();
+            this.RectangleButton = new System.Windows.Forms.ToolStripButton();
+            this.EllipseButton = new System.Windows.Forms.ToolStripButton();
+            this.ClearButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.thinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thickToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.DotButton,
+            this.LineButton,
+            this.CurveButton,
+            this.RectangleButton,
+            this.EllipseButton,
+            this.ClearButton,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // DotButton
+            // 
+            this.DotButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.DotButton.Image = ((System.Drawing.Image)(resources.GetObject("DotButton.Image")));
+            this.DotButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.DotButton.Name = "DotButton";
+            this.DotButton.Size = new System.Drawing.Size(23, 22);
+            this.DotButton.Text = "toolStripButton1";
+            // 
+            // LineButton
+            // 
+            this.LineButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.LineButton.Image = ((System.Drawing.Image)(resources.GetObject("LineButton.Image")));
+            this.LineButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(23, 22);
+            this.LineButton.Text = "toolStripButton2";
+            // 
+            // CurveButton
+            // 
+            this.CurveButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.CurveButton.Image = ((System.Drawing.Image)(resources.GetObject("CurveButton.Image")));
+            this.CurveButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.CurveButton.Name = "CurveButton";
+            this.CurveButton.Size = new System.Drawing.Size(23, 22);
+            this.CurveButton.Text = "toolStripButton3";
+            // 
+            // RectangleButton
+            // 
+            this.RectangleButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.RectangleButton.Image = ((System.Drawing.Image)(resources.GetObject("RectangleButton.Image")));
+            this.RectangleButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.RectangleButton.Name = "RectangleButton";
+            this.RectangleButton.Size = new System.Drawing.Size(23, 22);
+            this.RectangleButton.Text = "toolStripButton4";
+            // 
+            // EllipseButton
+            // 
+            this.EllipseButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.EllipseButton.Image = ((System.Drawing.Image)(resources.GetObject("EllipseButton.Image")));
+            this.EllipseButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.EllipseButton.Name = "EllipseButton";
+            this.EllipseButton.Size = new System.Drawing.Size(23, 22);
+            this.EllipseButton.Text = "toolStripButton5";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ClearButton.Image = ((System.Drawing.Image)(resources.GetObject("ClearButton.Image")));
+            this.ClearButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(23, 22);
+            this.ClearButton.Text = "toolStripButton6";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thinToolStripMenuItem,
+            this.normalToolStripMenuItem,
+            this.thickToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 22);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            // 
+            // thinToolStripMenuItem
+            // 
+            this.thinToolStripMenuItem.Name = "thinToolStripMenuItem";
+            this.thinToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.thinToolStripMenuItem.Text = "thin";
+            // 
+            // normalToolStripMenuItem
+            // 
+            this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
+            this.normalToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.normalToolStripMenuItem.Text = "normal";
+            // 
+            // thickToolStripMenuItem
+            // 
+            this.thickToolStripMenuItem.Name = "thickToolStripMenuItem";
+            this.thickToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.thickToolStripMenuItem.Text = "thick";
+            // 
+            // Paint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Paint";
+            this.Text = "Paint";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton DotButton;
+        private System.Windows.Forms.ToolStripButton LineButton;
+        private System.Windows.Forms.ToolStripButton CurveButton;
+        private System.Windows.Forms.ToolStripButton RectangleButton;
+        private System.Windows.Forms.ToolStripButton EllipseButton;
+        private System.Windows.Forms.ToolStripButton ClearButton;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem thinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem normalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thickToolStripMenuItem;
+    }
+}
+
---
+++ Team_Project_Paint/Team_Project_Paint/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Team_Project_Paint
+{
+    public partial class Paint : Form
+    {
+        public Paint()
+        {
+            InitializeComponent();
+        }
+    }
+}
