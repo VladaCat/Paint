dd1f4b4a3168aaa4b18497c1acdab5a7835755bc
Yana Naumova <naumovayana87@gmail.com>
2021-05-22 02:21:38 +0300

    add Hexagon and Round Rect
    Signed-off-by: Yana Naumova <naumovayana87@gmail.com>

---
+++ Team_Project_Paint/Team_Project_Paint/RoundingRect.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team_Project_Paint.Class;
+using Team_Project_Paint.Enum;
+using System.Drawing.Drawing2D;
+
+namespace Team_Project_Paint
+{
+    /*
+    https://www.codeproject.com/Articles/5649/Extended-Graphics-An-implementation-of-Rounded-Rec
+    */
+    public class RoundingRect : AbstractRectangleStyle
+    {
+        public RoundingRect() : base(NameForShapeFactory.RoundingRect) { }
+
+        int radius = 10;
+        public override void Draw(Graphics graphics)
+        {
+            int x = Location.X;
+            int y = Location.Y;
+            int width = FinishLocation.X - Location.X;
+            int height = FinishLocation.Y - Location.Y;
+            if (x > FinishLocation.X)
+            {
+                width = Math.Abs(FinishLocation.X - Location.X);
+                x = FinishLocation.X;
+            }
+            if (y > FinishLocation.Y)
+            {
+                height = Math.Abs(FinishLocation.Y - Location.Y);
+                y = FinishLocation.Y;
+            }
+
+            FinLocation = FinishLocation;
+            StartLocation = Location;
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Pen pen = new Pen(Color, Thickness);
+            pen.LineJoin =LineJoin.Round;
+            RectangleF rect = new RectangleF(x, y, width, height);
+            GraphicsPath path = this.GetRoundRectangle(rect, radius);
+            graphics.DrawPath(pen, path);
+        }
+
+        private GraphicsPath GetRoundRectangle(RectangleF baseRect, float radius)
+        {
+            if (radius <= 0.0F)
+            {
+                GraphicsPath mPath = new GraphicsPath();
+                mPath.AddRectangle(baseRect);
+                mPath.CloseFigure();
+                return mPath;
+            }
+
+            // if the corner radius is greater than or equal to 
+            // half the width, or height (whichever is shorter) 
+            // then return a capsule instead of a lozenge 
+            if (radius >= (Math.Min(baseRect.Width, baseRect.Height)) / 2.0)
+                return GetCapsule(baseRect);
+
+            // create the arc for the rectangle sides and declare 
+            // a graphics path object for the drawing 
+            float diameter = radius * 2.0F;
+            SizeF sizeF = new SizeF(diameter, diameter);
+            RectangleF arc = new RectangleF(baseRect.Location, sizeF);
+            GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+
+            // top left arc 
+            path.AddArc(arc, 180, 90);
+
+            // top right arc 
+            arc.X = baseRect.Right - diameter;
+            path.AddArc(arc, 270, 90);
+
+            // bottom right arc 
+            arc.Y = baseRect.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+
+            // bottom left arc
+            arc.X = baseRect.Left;
+            path.AddArc(arc, 90, 90);
+
+            path.CloseFigure();
+            return path;
+        }
+
+        private GraphicsPath GetCapsule(RectangleF baseRect)
+        {
+            float diameter;
+            RectangleF arc;
+            GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            try
+            {
+                if (baseRect.Width > baseRect.Height)
+                {
+                    // return horizontal capsule 
+                    diameter = baseRect.Height;
+                    SizeF sizeF = new SizeF(diameter, diameter);
+                    arc = new RectangleF(baseRect.Location, sizeF);
+                    path.AddArc(arc, 90, 180);
+                    arc.X = baseRect.Right - diameter;
+                    path.AddArc(arc, 270, 180);
+                }
+                else if (baseRect.Width < baseRect.Height)
+                {
+                    // return vertical capsule 
+                    diameter = baseRect.Width;
+                    SizeF sizeF = new SizeF(diameter, diameter);
+                    arc = new RectangleF(baseRect.Location, sizeF);
+                    path.AddArc(arc, 180, 180);
+                    arc.Y = baseRect.Bottom - diameter;
+                    path.AddArc(arc, 0, 180);
+                }
+                else
+                {
+                    // return circle 
+                    path.AddEllipse(baseRect);
+                }
+            }
+            catch (Exception ex)
+            {
+                path.AddEllipse(baseRect);
+            }
+            finally
+            {
+                path.CloseFigure();
+            }
+            return path;
+        }
+    }
+}
+
