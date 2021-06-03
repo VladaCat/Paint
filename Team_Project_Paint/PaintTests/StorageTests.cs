19dc3001df9a3e0ce1b6c56d9424c5687529f58c
Ilya <fmgorod@gmail.com>
2021-06-04 01:53:24 +0300

    Delete using

--- Team_Project_Paint/PaintTests/StorageTests.cs
+++ Team_Project_Paint/PaintTests/StorageTests.cs
@@ -1,10 +1,6 @@
 ﻿using Moq;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Team_Project_Paint.Class;
 using Team_Project_Paint.Class.OperationWithFigures;
 using Team_Project_Paint.Interfaces;
