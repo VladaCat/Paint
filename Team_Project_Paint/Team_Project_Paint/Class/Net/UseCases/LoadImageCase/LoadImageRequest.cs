fad37d4c8b41ac57402d02f378f8c34a02ae8e32
Yana Naumova <naumovayana87@gmail.com>
2021-06-29 20:31:59 +0300

    small cleanup

--- Team_Project_Paint/Team_Project_Paint/Class/Net/UseCases/LoadImageCase/LoadImageRequest.cs
+++ Team_Project_Paint/Team_Project_Paint/Class/Net/UseCases/LoadImageCase/LoadImageRequest.cs
@@ -9,61 +9,61 @@ using System.Threading.Tasks;
 
 namespace Team_Project_Paint.Net
 {
-    public class LoadImageRequest : ILoadImageRequest
-    {
-        private LoadImageInfo _loadImageInfo;
-        private string _paintServerUrl;
-        private IRestClient _restClient;
-        public LoadImageResultData LastLoadImageResultData { get; private set; }
-        public HttpStatusCode LastHttpStatusCode { get; private set; }
+    //public class LoadImageRequest : ILoadImageRequest
+    //{
+    //    private LoadImageInfo _loadImageInfo;
+    //    private string _paintServerUrl;
+    //    private IRestClient _restClient;
+    //    public LoadImageResultData LastLoadImageResultData { get; private set; }
+    //    public HttpStatusCode LastHttpStatusCode { get; private set; }
 
-        public LoadImageRequest(LoadImageInfo loadImageInfo, string paintServerUrl)
-        {
-            _loadImageInfo = loadImageInfo;
-            _paintServerUrl = paintServerUrl;
-        }
+    //    public LoadImageRequest(LoadImageInfo loadImageInfo, string paintServerUrl)
+    //    {
+    //        _loadImageInfo = loadImageInfo;
+    //        _paintServerUrl = paintServerUrl;
+    //    }
 
-        public bool Execute()
-        {
-            var request = new RestRequest { Resource = $"{_paintServerUrl}/operation/load", Method = Method.POST };
+    //    public bool Execute()
+    //    {
+    //        var request = new RestRequest { Resource = $"{_paintServerUrl}/operation/load", Method = Method.POST };
 
 
-            request.AddJsonBody(_loadImageInfo);
+    //        request.AddJsonBody(_loadImageInfo);
 
-            _restClient = new RestClient();
-            var response = _restClient.Execute(request);
+    //        _restClient = new RestClient();
+    //        var response = _restClient.Execute(request);
 
-            LastHttpStatusCode = response.StatusCode;
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                try
-                {
-                    var loadImageResultData = new JsonSerializer().Deserialize<LoadImageResultData>(response);
+    //        LastHttpStatusCode = response.StatusCode;
+    //        if (response.StatusCode == HttpStatusCode.OK)
+    //        {
+    //            try
+    //            {
+    //                var loadImageResultData = new JsonSerializer().Deserialize<LoadImageResultData>(response);
 
-                    LastLoadImageResultData = loadImageResultData;
+    //                LastLoadImageResultData = loadImageResultData;
 
-                    if (loadImageResultData.LoadImageResult == true)
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
+    //                if (loadImageResultData.LoadImageResult == true)
+    //                {
+    //                    return true;
+    //                }
+    //                else
+    //                {
+    //                    return false;
+    //                }
 
-                }
-                catch
-                {
-                    LastLoadImageResultData = null;
-                    return false;
-                }
+    //            }
+    //            catch
+    //            {
+    //                LastLoadImageResultData = null;
+    //                return false;
+    //            }
 
-            }
-            else
-            {
-                LastLoadImageResultData = null;
-                return false;
-            }
-        }
-    }
+    //        }
+    //        else
+    //        {
+    //            LastLoadImageResultData = null;
+    //            return false;
+    //        }
+    //    }
+    //}
 }
